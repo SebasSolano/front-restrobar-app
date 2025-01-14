a5b90cd5bbbf4a9d2b47bf8f3808089d3a9614fe
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-14 17:23:08 -0500

    added: logic for image-list

--- src/app/components/image-list/image-list.component.ts
+++ src/app/components/image-list/image-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-image-list',
   templateUrl: './image-list.component.html',
-  styleUrls: ['./image-list.component.css']
+  styleUrls: ['./image-list.component.css'],
 })
 export class ImageListComponent implements OnInit {
+  uploadedImages: any[] = [];
+  startDate: string = '';
+  endDate: string = '';
 
-  constructor() { }
+  constructor(private imageService: ImageService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  fetchImage() {
+    if (this.startDate && this.endDate) {
+      this.imageService
+        .getImagesBetweenDates(this.startDate, this.endDate)
+        .subscribe({
+          next: (images) => {
+            this.uploadedImages = images;
+          },
+          error: (err) => console.error(err),
+        });
+    } else {
+      alert('Please select start and end dates.');
+    }
   }
-
 }
