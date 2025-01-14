70cd9f0c8b7933fbb1b634e299f21ae7b5e85a41
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-13 19:39:58 -0500

    project initialization

---
+++ src/app/components/image-stats/image-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-stats',
+  templateUrl: './image-stats.component.html',
+  styleUrls: ['./image-stats.component.css']
+})
+export class ImageStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
