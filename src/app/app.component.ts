9f032d2d72fa1c2cbee1dd494eab4cdc65166a9b
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-14 17:40:59 -0500

    added: html components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ImageUploadComponent } from './components/image-upload/image-upload.component';
 import { ImageListComponent } from "./components/image-list/image-list.component";
+import { ImageStatsComponent } from "./components/image-stats/image-stats.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HttpClientModule, FormsModule, ImageUploadComponent, ImageListComponent],
+  imports: [HttpClientModule, FormsModule, ImageUploadComponent, ImageListComponent, ImageStatsComponent],
   template: `
     <div class="container mx-auto">
       <h1 class="text-4xl font-bold text-center mt-10">
@@ -15,6 +16,7 @@ import { ImageListComponent } from "./components/image-list/image-list.component
 
       <app-image-upload></app-image-upload>
       <app-image-list></app-image-list>
+      <app-image-stats></app-image-stats>
     </div>
   `,
   styleUrl: './app.component.css',
--- src/app/components/image-stats/image-stats.component.ts
+++ src/app/components/image-stats/image-stats.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../../services/image.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-image-stats',
   templateUrl: './image-stats.component.html',
+  imports: [NgFor],
   styleUrls: ['./image-stats.component.css'],
 })
 export class ImageStatsComponent implements OnInit {
