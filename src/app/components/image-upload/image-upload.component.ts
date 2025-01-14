1e5aaf8178a5cc8bcd72af5c38f21f073a81fdfb
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-14 17:18:32 -0500

    added: html content and miscellaneous fixes

--- src/app/components/image-upload/image-upload.component.ts
+++ src/app/components/image-upload/image-upload.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../../services/image.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-image-upload',
+  imports: [FormsModule],
   templateUrl: './image-upload.component.html',
   styleUrls: ['./image-upload.component.css'],
 })
