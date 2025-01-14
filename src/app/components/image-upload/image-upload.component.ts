696638dcd84e6576c40383d4fac6939ef215cf63
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-14 16:44:28 -0500

    added: logic for image-upload

--- src/app/components/image-upload/image-upload.component.ts
+++ src/app/components/image-upload/image-upload.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-image-upload',
   templateUrl: './image-upload.component.html',
-  styleUrls: ['./image-upload.component.css']
+  styleUrls: ['./image-upload.component.css'],
 })
 export class ImageUploadComponent implements OnInit {
+  imageFile: File | null = null;
+  name: string = '';
 
-  constructor() { }
+  constructor(private imageService: ImageService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onFileSelected(event: any) {
+    this.imageFile = event.target.files[0];
   }
 
+  uploadImage() {
+    if (this.imageFile && this.name) {
+      this.imageService.uploadImage(this.imageFile, this.name).subscribe({
+        next: () => alert('Image uploaded successfully!'),
+        error: (err) => console.error(err),
+      });
+    } else {
+      alert('Please select an image and enter a name');
+    }
+  }
 }
