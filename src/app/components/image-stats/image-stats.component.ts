626a56e233b2e906f5ad2de2cfad9b065356db55
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-14 17:36:41 -0500

    added: image-stats logics

--- src/app/components/image-stats/image-stats.component.ts
+++ src/app/components/image-stats/image-stats.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-image-stats',
   templateUrl: './image-stats.component.html',
-  styleUrls: ['./image-stats.component.css']
+  styleUrls: ['./image-stats.component.css'],
 })
 export class ImageStatsComponent implements OnInit {
+  stats: any[] = [];
+  constructor(private imageService: ImageService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  fetchStats() {
+    this.imageService.getImagesStats().subscribe({
+      next: (stats) => {
+        this.stats = stats;
+      },
+      error: (err) => console.error(err),
+    });
   }
-
 }
