2eda8629f2840c389ae21d8f83ae02bbcccf480d
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-14 22:27:34 -0500

    improved: styles of statistics

--- src/app/components/image-stats/image-stats.component.ts
+++ src/app/components/image-stats/image-stats.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../../services/image.service';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-image-stats',
   templateUrl: './image-stats.component.html',
-  imports: [NgFor],
+  imports: [NgFor, CommonModule],
   styleUrls: ['./image-stats.component.css'],
 })
 export class ImageStatsComponent implements OnInit {
   stats: any[] = [];
   constructor(private imageService: ImageService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchStats()
+  }
 
   fetchStats() {
     this.imageService.getImagesStats().subscribe({
       next: (stats) => {
         this.stats = stats;
+        this.stats = this.stats.sort((a, b) => a._id - b._id);
       },
       error: (err) => console.error(err),
     });
