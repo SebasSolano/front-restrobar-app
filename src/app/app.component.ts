7f4e82d87329f183c8fe5f94bb3a795868146a17
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-14 22:42:51 -0500

    added: image deletion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ImageUploadComponent } from './components/image-upload/image-upload.component';
 import { RouterModule } from '@angular/router';
+import { ImageService } from './services/image.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,20 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private imageService: ImageService) {}
+
+  deleteAllImages() {
+    if (confirm('Are you sure you want to delete all images?')) {
+      this.imageService.deleteAllImages().subscribe({
+        next: (response) => {
+          alert(response.message);
+        },
+        error: (err) => {
+          console.error(err);
+          alert('An error occurred while deleting images');
+        },
+      });
+    }
+  }
+}
