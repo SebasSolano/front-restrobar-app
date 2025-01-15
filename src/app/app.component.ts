ecf25a132e40f31d41e1ee5c363f75448f656a26
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-14 21:32:19 -0500

    improved: app and image-upload styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ImageUploadComponent } from './components/image-upload/image-upload.component';
-import { ImageListComponent } from "./components/image-list/image-list.component";
-import { ImageStatsComponent } from "./components/image-stats/image-stats.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HttpClientModule, FormsModule, ImageUploadComponent, ImageListComponent, ImageStatsComponent],
-  template: `
-    <div class="container mx-auto">
-      <h1 class="text-4xl font-bold text-center mt-10">
-        Image Processor- RestoBar
-      </h1>
-
-      <app-image-upload></app-image-upload>
-      <app-image-list></app-image-list>
-      <app-image-stats></app-image-stats>
-    </div>
-  `,
+  imports: [HttpClientModule, FormsModule, ImageUploadComponent],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {}
