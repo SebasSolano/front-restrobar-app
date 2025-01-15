b75b2787814010a270a6806035b19e8730b88883
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-14 21:36:09 -0500

    added: route management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ImageUploadComponent } from './components/image-upload/image-upload.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HttpClientModule, FormsModule, ImageUploadComponent],
+  imports: [HttpClientModule, FormsModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ImageListComponent } from './components/image-list/image-list.component';
+import { ImageUploadComponent } from './components/image-upload/image-upload.component';
+import { ImageStatsComponent } from './components/image-stats/image-stats.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ImageUploadComponent },
+  { path: 'list', component: ImageListComponent },
+  { path: 'stats', component: ImageStatsComponent },
+];
